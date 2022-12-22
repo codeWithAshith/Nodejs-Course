edd8dc5d4af0b0df0697dfb149fc1b11edc344ac
codewithashith <codewithashith@gmail.com>
2022-12-22 18:03:35 +0530

    4.File System

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
-// to run -> "node app"
+const fs = require("fs");
 
-// import single item
-// const users = require("./users");
+// read from a file
+// fs.readFile("./note.txt", (err, data) => {
+//   if (err) console.log(err);
+//   else console.log(data.toString());
+// });
 
-// console.log(users);
+// write to a file - override
+// fs.writeFile("./note.txt", "A new line", (err, data) => {
+//   if (err) console.log(err);
+//   else console.log("A new line has been added");
+// });
 
-// import multiple things
-// const multiple = require("./users");
-// console.log(multiple.numbers);
+// append to a file - add line to an existing file
+// fs.appendFile("./note.txt", "A new line\r\n", (err, data) => {
+//   if (err) console.log(err);
+//   else console.log("A new line has been added");
+// });
 
+// make new folder
+// fs.mkdir("newFolder", (err, data) => {
+//   if (err) console.log(err);
+//   else console.log("A new folder has been created");
+// });
 
-// import multiple things using destructing
-const { numbers, string } = require("./users");
-console.log(numbers);
-console.log(string);
+// check if file already exists
+// if (!fs.existsSync("newFolder")) {
+//   fs.mkdir("newFolder", (err, data) => {
+//     if (err) console.log(err);
+//     else console.log("A new folder has been created");
+//   });
+// } else {
+//   console.log("The folder already exists!!!");
+// }
+
+// Delete folder
+// if (fs.existsSync("newFolder")) {
+//   fs.rmdir("newFolder", (err, data) => {
+//     if (err) console.log(err);
+//     else console.log("The folder has been deleted");
+//   });
+// } else {
+//   console.log("The folder already deleted!!!");
+// }
+
+// Delete file
+if (fs.existsSync("./note.txt")) {
+  fs.unlink("./note.txt", (err, data) => {
+    if (err) console.log(err);
+    else console.log("The file has been deleted");
+  });
+} else {
+  console.log("The file already deleted!!!");
+}
