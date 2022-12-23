e31db79b9b0f668216fe116094fc4b2f920c944c
codewithashith <codewithashith@gmail.com>
2022-12-23 13:49:00 +0530

    6.Server Request

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const http = require("http");
 const port = 3000;
 const hostName = "localhost";
 
+// To avoid restarting the server after every change
+// Install nodemon
+// npm install -g nodemon
+
 const server = http.createServer((req, res) => {
-  console.log("A request is made");
+  console.log(req);
+  // Try localhost:3000 and also localhost:3000/contact
+  console.log(`Request url -> ${req.url}`);
+  console.log(`Request method -> ${req.method}`);
 });
 
 // Enter localhost:3000 to see the log -> A request is made
