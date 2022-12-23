fe2ea645757fcfe86c477e6b704a318d31b71430
codewithashith <codewithashith@gmail.com>
2022-12-23 14:33:16 +0530

    8.Returning html file

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 const http = require("http");
+const fs = require("fs");
 
 const port = 3000;
 const hostName = "localhost";
 
-// To avoid restarting the server after every change
-// Install nodemon
-// npm install -g nodemon
-
 const server = http.createServer((req, res) => {
-  // To send plain text as response
-  // Go to http://localhost:3000/ to see the response
-
-  //   res.setHeader("Content-Type", "text/plain");
-  //   res.write("Welcome to Code with Ashith");
-  //   res.end();
-
-  // To send plain html as response
-  // Go to http://localhost:3000/ to see the response
   res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Welcome to Code with Ashith</h1>");
-  res.write("<p>This is a Nodejs Application</p>");
-  res.end();
+  fs.readFile("./index.html", (error, data) => {
+    if (error) console.log(error);
+    else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 // Enter localhost:3000 to see the log -> A request is made
